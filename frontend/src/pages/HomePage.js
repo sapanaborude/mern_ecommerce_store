9c132e99a4f23f3e0e01bf99fa91bf5af98c599b
borudesc <borudesapana1998@gmail.com>
2024-09-04 13:47:52 +0530

    updated category file

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -23,7 +23,7 @@ const HomePage = () => {
   //get all cat
   const getAllCategory = async () => {
     try {
-      const { data } = await axios.get("https://mern-ecommerce-backend-c87p.onrender.com/api/v1category/get-category");
+      const { data } = await axios.get("https://mern-ecommerce-backend-c87p.onrender.com/api/v1/category/get-category");
       if (data?.success) {
         setCategories(data?.category);
       }
